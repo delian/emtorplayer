6184359806f0de4aeef3c89c48a54299590c5766
Delian Delchev <delian.delchev@gmail.com>
2016-02-20 21:44:19 +0100

    Small restructure
    Signed-off-by: Delian Delchev <delian.delchev@gmail.com>

--- main.js
+++ main.js
@@ -15,6 +15,9 @@ function createWindow () {
   mainWindow = new BrowserWindow({width: 800, height: 600});
 
   // and load the index.html of the app.
+  
+  console.log('dir',__dirname);
+  
   mainWindow.loadURL('file://' + __dirname + '/index.html');
 
   // Open the DevTools.
