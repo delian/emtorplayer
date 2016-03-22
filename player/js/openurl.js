a2578af27df76d1d61b728d231ef7fc3925ef539
Delian Delchev <delian.delchev@gmail.com>
2016-03-22 14:26:23 +0100

    updates
    Signed-off-by: Delian Delchev <delian.delchev@gmail.com>

--- player/js/openurl.js
+++ player/js/openurl.js
@@ -1,16 +1,21 @@
 var Init = require('./init');
 
+function submit() {
+    var url = 'file://'+window.location.pathname+'?magnet='+escape($('#openurlForm').val());
+    console.log('Will go for',url);
+    window.location.href = url;
+}
+
 function OpenURL() {
+    
+    $('#openurlFormID').submit(submit);
+    
     $('#openurl').dialog({
         modal: true,
         width: '95%',
         height: 200,
         buttons: {
-            "Open": function() {
-                var url = 'file://'+window.location.pathname+'?magnet='+escape($('#openurlForm').val());
-                console.log('Will go for',url);
-                window.location.href = url;
-            },
+            "Open": submit,
             "Cancel": function() {
                 $(this).dialog("close");
             }
